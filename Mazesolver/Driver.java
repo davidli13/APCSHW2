febfc4a63a2b3160433d6025d0c295958b00e0ed
davidli13 <davidli.stuy@gmail.com>
2015-02-11 13:09:28 -0500

    looks

--- Mazesolver/Driver.java
+++ Mazesolver/Driver.java
@@ -3,7 +3,7 @@ public class Driver{
     public static void main(String[]args){
 	Mazesolver f;
 	if(args.length < 1){
-	    f = new Mazesolver("data3.dat");
+	    f = new Mazesolver("data1.dat");
 	}else{
 	    f = new Mazesolver(args[0]);
 	}
--- Mazesolver/Mazesolver.java
+++ Mazesolver/Mazesolver.java
@@ -1,70 +1,65 @@
 import java.util.*;
 import java.io.*;
-public class Mazesolver{
 
+public class Mazesolver{
     private char[][]maze;
     private int maxx,maxy;
     private int startx,starty;
+    //Terminal keycodes to clear the terminal, or hide/show the cursor
+    private static String clear =  "\033[2J";
+    private static String hide =  "\033[?25l";
+    private static String show =  "\033[?25h";
 
     public Mazesolver(String filename){
 	startx = -1;
 	starty = -1;
+	//read the whole maze into a single string first
 	String ans = "";
 	try{
 	    Scanner in = new Scanner(new File(filename));
 
 	    //keep reading next line
 	    while(in.hasNext()){
-		String line= in.nextLine();
-		if(maxy==0){
+		String line = in.nextLine();
+		if(maxy == 0){
 		    //calculate width of the maze
-		    maxx=line.length();
+		    maxx = line.length();
 		}
 		//every new line add 1 to the height of the maze
 		maxy++;
-		ans+=line;
+		ans += line;
 	    }
 	}
 	catch(Exception e){
-	    System.out.println("File: "+filename+" could not be opened.");
+	    System.out.println("File: " + filename + " could not be opened.");
 	    e.printStackTrace();
 	    System.exit(0);
 	}
 
+	//copy from the single string to a 2D array
 	maze = new char[maxx][maxy];
-	for(int i=0;i<ans.length();i++){
+	for(int i = 0; i < ans.length(); i++){
 	    char c = ans.charAt(i);
-	    maze[i%maxx][i/maxx]= c;
-	    if(c=='S'){
-		startx = i%maxx;
-		starty = i/maxx;
+	    maze[i % maxx][i / maxx] = c;
+	    if(c == 'S'){
+		startx = i % maxx;
+		starty = i / maxx;
 	    }
 	}
     }
 
+    //terminal specific character to move the cursor
     private String go(int x,int y){
-	return ("["+x+";"+y+"H");
+	return ("\033[" + x + ";" + y + "H");
     }
     
-    private String clear(){
-	return  "[2J";
-    }
 
-    private String hide(){
-	return  "[?25l";
+    private String color(int foreground,int background){
+	return ("\033[0;" + foreground + ";" + background + "m");
     }
-
-    private String show(){
-	return  "[?25h";
-    }
-    private String invert(){
-	return  "[37";
-    }
-
-
 
     public void clearTerminal(){
-	System.out.println(clear());
+	System.out.println(clear);
     }
 
     public void wait(int millis){
@@ -76,44 +71,50 @@ public class Mazesolver{
     }
 
     public String toString(){
-	String ans = ""+maxx+","+maxy+"\n";
-	for(int i=0;i<maxx*maxy;i++){
-	    if(i%maxx ==0 && i!=0){
-		ans+="\n";
+	String ans = "Solving a maze that is " + maxx + " by " + maxy + "\n";
+	for(int i = 0; i < maxx * maxy; i++){
+	    if(i % maxx == 0 && i != 0){
+		ans += "\n";
 	    }
-	    ans += maze[i%maxx][i/maxx];
+	    char c =  maze[i % maxx][i / maxx];
+	    if(c == '#'){
+		ans += color(38,47)+c;
+	    }else{
+		ans += color(32,40)+c;
+	    }
 	}
-	return hide()+invert()+go(0,0)+ans+"\n"+show();
+	return hide + go(0,0) + ans + "\n" + show + color(37,40);
     }
     
-    public void solve(){
+    public boolean solve(){
 	if(startx < 0){
 	    System.out.println("No starting point 'S' found in maze.");
+	    return false;
 	}else{
-	    maze[startx][starty]=' ';
-	    solve(maze,startx,starty);
+	    maze[startx][starty] = ' ';
+	    return solve(startx,starty);
 	}
     }
 
-    public boolean solve(char[][]maze,int x,int y){
+    public boolean solve(int x,int y){
 	System.out.println(this);
 	wait(20);
-	//ASSIGNMENT IS TO COMPLETE THIS PART******************
-	if (maze[x][y] == 'E'){
+	if(maze[x][y] == 'E'){
 	    return true;
 	}
-	if (maze[x][y] == ' '){
+	if(maze[x][y] == ' '){
+	    //mark the floor with @
 	    maze[x][y] = '@';
-	    if(solve(maze,x+1,y) ||
-	       solve(maze,x-1,y) ||
-	       solve(maze,x,y+1) ||
-	       solve(maze,x,y-1)){
+	    //recursion ho!!
+	    if( solve(x + 1,y) ||
+		solve(x,y + 1) ||
+		solve(x - 1,y) ||
+		solve(x,y - 1)){
 		return true;
 	    }
+	    //replace the @ with a .
 	    maze[x][y] = '.';
-	
 	}
-	//ASSIGNMENT IS TO COMPLETE THE PART ABOVE THIS******************
 	return false;//by default the maze didn't get solved
     }
 
