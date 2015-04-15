9ac709c4b59381c9b4127c4a82f456562ff27e0e
davidli13 <davidli.stuy@gmail.com>
2015-04-15 13:14:21 -0400

    d

--- 11BFS/MyDeque.java
+++ 11BFS/MyDeque.java
@@ -1,85 +1,160 @@
 import java.util.*;
+import java.io.*;
 
-public class MyDeque<T>{
-    int head,tail,size;
-    Object[] ary;
-    
-    public MyDeque(){
-	ary = new Object[6];
-	head = 2;
-	tail = 3;
+public class MyDeque<T> {
+
+    private T[] items;
+    private int head;
+    private int tail;
+    private int size;    
+
+    public MyDeque() {
+	items = (T[]) (new Object[10]);
+	head = 0;
+	tail = 9;
 	size = 0;
     }
-
-    public void addFirst(T value){
-	if (size == ary.length){
-	    ary = resize(ary);
-	}
-	ary[head] = value;
-	head--;
-	size++;
-	if (head == -1){
-	    head = ary.length-1;
-	}
+    public int getHead(){
+	return head;
+    }
+    public int getTail(){
+	return tail;
     }
 
-    public void addLast(T value){
-	if (size == ary.length){
-	    ary = resize(ary);
+    private int normalize(int n) {
+	while (n < items.length) {
+	    n += items.length;
 	}
-	ary[tail] = value;
-	tail++;
-	size++;
-	if (tail == ary.length){
-	    tail = 0;
-	}
+	return n % items.length;
     }
 
-    public Object[] resize(Object[] ary){
-	Object[] clone = new Object[ary.length*2];
-	for (int i = 1;i<ary.length-head;i++){
-	    clone[ary.length/2+i-1] = ary[head+i];
+    private void resize() {
+	int newSize = size;
+	if (size == items.length) {
+	    newSize = size * 2;
+	} else {
+	    return;
 	}
-	for (int i = 0;i<head+1;i++){
-	    clone[ary.length/2+ary.length-head+i-1] = ary[i];
-	}
-	head = size/2-1;
-	tail = size*3/2;
-	return clone;
-    }
-
-    public T removeFirst(){
-	if (head == ary.length -1){
-	    head = 0;
-	}else{
-	    head++;
-	}
-	T save = (T)ary[head];
-	ary[head] = null;
-	return save;
-    }
-
-    public T removeLast(){
-	if (tail == 0){
-	    tail = ary.length-1;
-	}else{
-	    tail--;
-	}
-	T save = (T)ary[tail];
-	ary[tail] = null;
-	return save;
-    }
     
-    public String toString(){
-	String result = "";
-	for (int i = 1;i<ary.length;i++){
-	    if (ary[(head+i)%ary.length] != null){
-		if (i > 1){
-		    result += ", ";
-		}
-		result += ary[(head+i)%ary.length];
+	T[] newArray = (T[]) (new Object[newSize]);
+	int copyCounter = 0;
+	if (head <= tail) {
+	    for (int i = head; i <= tail; ++i) {
+		newArray[copyCounter] = items[i];
+		copyCounter++;
+	    }
+	} else {
+	    for (int i = head; i < items.length; ++i) {
+		newArray[copyCounter] = items[i];
+		copyCounter++;
+	    }
+	    for (int i = 0; i <= tail; ++i) {
+		newArray[copyCounter] = items[i];
+		copyCounter++;
 	    }
 	}
-	return result;
-    }	
+	head = 0;
+	tail = size - 1;
+	items = newArray;
+    }
+
+    public boolean add(T item) {
+	addLast(item);
+	return true;
+    }
+    public void add(T item, int priority){
+	resize();
+	addLast(item);
+	size++;
+    }
+
+    public void addFirst(T item) {
+	resize();
+	head = normalize(head - 1);
+	items[head] = item;
+	size++;
+    }
+
+    public void addLast(T item) {
+	resize();
+	tail = normalize(tail + 1);
+	items[tail] = item;
+	size++;
+    }
+    public T get(int index){
+	if (head < tail){
+	    if (index < head || index > tail){
+		throw new ArrayIndexOutOfBoundsException();
+	    }
+	}else{
+	    if (index > tail && index < head){
+		throw new ArrayIndexOutOfBoundsException();
+	    }
+	}
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	return items[index];
+    }
+    public T getFirst() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	return items[head];
+    }
+
+    public T getLast() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	return items[tail];
+    }
+
+    public T removeFirst() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	size--;
+	int index = head;
+	head = normalize(head + 1);
+	return items[index];
+    }
+
+    public T removeLast() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	size--;
+	int index = tail;
+	tail = normalize(tail - 1);
+	return items[index];
+    }
+    
+    public T removeSmallest(){
+	return items[0];
+    }
+
+    public int size() {
+	return size;
+    }
+
+    public String toString() {
+	if (size == 0) {
+	    return "[ ]";
+	}
+	String out = "[ ";
+	if (head <= tail) {
+	    for (int i = head; i <= tail; ++i) {
+		out += items[i] + " ";
+	    }
+	} else {
+	    for (int i = head; i < items.length; ++i) {
+		out += items[i] + " ";
+	    }
+	    for (int i = 0; i <= tail; ++i) {
+		out += items[i] + " ";
+	    }
+	}
+	return out + "]";
+    }
 }
